966a041088cd96bc4e492d8560089b2a4dd22fac
ArtemE54 <erosuinartem@gmail.com>
2024-12-11 19:34:40 +0700

    mergesirt.py

--- chapter 7/mergesirt.py
+++ chapter 7/mergesirt.py
@@ -1,25 +1,21 @@
 def merge_sort(arr):
     if len(arr) <= 1:
         return arr
-
-    # Разделяем массив на две половины
+        
     mid = len(arr) // 2
     left_half = arr[:mid]
     right_half = arr[mid:]
 
-    # Рекурсивно сортируем каждую половину
     left_half = merge_sort(left_half)
     right_half = merge_sort(right_half)
 
-    # Объединяем отсортированные половины
     return merge(left_half, right_half)
 
 
 def merge(left, right):
     sorted_arr = []
     left_index, right_index = 0, 0
-
-    # Сравниваем элементы из двух отсортированных массивов и добавляем меньший элемент в новый массив
+    
     while left_index < len(left) and right_index < len(right):
         if left[left_index] < right[right_index]:
             sorted_arr.append(left[left_index])
@@ -27,11 +23,7 @@ def merge(left, right):
         else:
             sorted_arr.append(right[right_index])
             right_index += 1
-
-    # Добавляем оставшиеся элементы из левой половины
     sorted_arr.extend(left[left_index:])
-
-    # Добавляем оставшиеся элементы из правой половины
     sorted_arr.extend(right[right_index:])
 
     return sorted_arr
